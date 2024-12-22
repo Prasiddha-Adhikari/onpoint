0d0809b348f4c0bc40863d0b59fea2ef9a656c37
Prasid <97814916+Prasiddha-Adhikari@users.noreply.github.com>
2024-12-22 20:36:35 +0545

    changed

--- backend/api/db.php
+++ backend/api/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "onpointeducation"; // Replace with your database username
-$password = "Design@2025"; // Replace with your database password
-$dbname = "onpointeducation_php122"; // Replace with your database name
+$username = "username"; // Replace with your database username
+$password = "passsword"; // Replace with your database password
+$dbname = "dbname"; // Replace with your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
